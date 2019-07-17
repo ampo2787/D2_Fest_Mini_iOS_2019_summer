818a058c274bbb55e68b1e909b48ec280c873679
JIHOON PARK <ampo2787@cnu.ac.kr>
2019-07-17 20:26:10 +0900

    Project Init, Camera Function add

---
+++ CinemaGraph/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  CinemaGraph
+//
+//  Created by JihoonPark on 16/07/2019.
+//  Copyright © 2019 JihoonPark. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var cameraController:UIImagePickerController! = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.cameraController = UIImagePickerController.init()
+        self.cameraController.delegate = self
+    }
+
+    @IBAction func takePictureBtnClicked(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            self.cameraController.sourceType = UIImagePickerController.SourceType.camera
+            self.cameraController.allowsEditing = true
+            self.cameraController.mediaTypes = UIImagePickerController.availableMediaTypes(for: UIImagePickerController.SourceType.camera)!
+            
+            self.present(self.cameraController, animated: true, completion: nil)
+        }
+        else {
+            let alertView = UIAlertController(title: "Error", message: "No Camera", preferredStyle: UIAlertController.Style.actionSheet)
+            alertView.addAction(UIAlertAction(title:"OK", style: .default, handler: nil))
+            self.present(alertView, animated: true, completion: nil)
+        }
+        
+    }
+    func takePicture(){
+        if self.cameraController.cameraCaptureMode == UIImagePickerController.CameraCaptureMode.photo {
+            let alertView = UIAlertController(title: "Error", message: "동영상을 촬영해야 합니다.", preferredStyle: UIAlertController.Style.actionSheet)
+            alertView.addAction(UIAlertAction(title: "OK", style: .default, handler: { (UIAlertAction) in
+                self.cameraController.cameraCaptureMode = UIImagePickerController.CameraCaptureMode.video
+            }))
+            self.present(alertView, animated: true, completion: nil)
+        }
+        else {
+            let isSuccess = self.cameraController.startVideoCapture()
+            
+            if !isSuccess {
+                self.cameraController.stopVideoCapture()
+            }
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let MediaType = info[UIImagePickerController.InfoKey.mediaType] as? String {
+            if MediaType == (kUTTypeImage as String){
+                let alertView = UIAlertController(title: "Error", message: "동영상을 편집해야 합니다.", preferredStyle: UIAlertController.Style.actionSheet)
+                alertView.addAction(UIAlertAction(title:"OK", style: .default, handler: nil))
+                self.present(alertView, animated: true, completion: nil)
+            }
+            else {
+                if let videoURL = info[UIImagePickerController.InfoKey.mediaURL] as? URL {
+                    UISaveVideoAtPathToSavedPhotosAlbum(videoURL.relativePath, nil, nil, nil)
+                }
+            }
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    
+}
+
