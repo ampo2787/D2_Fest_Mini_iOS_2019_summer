17bbb53b41d885606a6cd42912d92873385f461a
JIHOON PARK <ampo2787@cnu.ac.kr>
2019-07-19 21:04:22 +0900

    Camera And Album can select video only

--- CinemaGraph/Controller/StartVC.swift
+++ CinemaGraph/Controller/StartVC.swift
@@ -9,20 +9,26 @@
 import UIKit
 import MobileCoreServices
 
-class ViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class StartVC: UIViewController  {
     var cameraController:UIImagePickerController! = nil
     
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         self.cameraController = UIImagePickerController.init()
         self.cameraController.delegate = self
     }
 
+    // MARK: - Button Action
+    /*
+     Camera Open Only Video Mode.
+     Show Alert View For No Camera Model
+     */
     @IBAction func takePictureBtnClicked(_ sender: UIButton) {
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
-            self.cameraController.sourceType = UIImagePickerController.SourceType.camera
-            self.cameraController.allowsEditing = true
-            self.cameraController.mediaTypes = UIImagePickerController.availableMediaTypes(for: UIImagePickerController.SourceType.camera)!
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            self.cameraController.sourceType = .camera
+            self.cameraController.allowsEditing = false
+            self.cameraController.mediaTypes = [kUTTypeMovie as String]
             
             self.present(self.cameraController, animated: true, completion: nil)
         }
@@ -33,25 +39,22 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate, UINavi
         }
         
     }
-    func takePicture(){
-        if self.cameraController.cameraCaptureMode == UIImagePickerController.CameraCaptureMode.photo {
-            let alertView = UIAlertController(title: "Error", message: "동영상을 촬영해야 합니다.", preferredStyle: UIAlertController.Style.actionSheet)
-            alertView.addAction(UIAlertAction(title: "OK", style: .default, handler: { (UIAlertAction) in
-                self.cameraController.cameraCaptureMode = UIImagePickerController.CameraCaptureMode.video
-            }))
-            self.present(alertView, animated: true, completion: nil)
-        }
-        else {
-            let isSuccess = self.cameraController.startVideoCapture()
-            
-            if !isSuccess {
-                self.cameraController.stopVideoCapture()
-            }
-        }
+    /*
+     Album Open Only Video Mode.
+     */
+    @IBAction func videoSelectFromAlbumBtnClicked(_ sender: UIButton) {
+        self.cameraController.sourceType = .photoLibrary
+        self.cameraController.mediaTypes = [kUTTypeMovie as String]
+        self.present(self.cameraController, animated: true, completion: nil)
     }
     
+    // MARK: - ImagePickerDelegate
+    /*
+    This Function Play After Video Capture
+    If Data type is Photo, show alertView.
+    else, Video Save UserDefaults.
+    */
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
         if let MediaType = info[UIImagePickerController.InfoKey.mediaType] as? String {
             if MediaType == (kUTTypeImage as String){
                 let alertView = UIAlertController(title: "Error", message: "동영상을 편집해야 합니다.", preferredStyle: UIAlertController.Style.actionSheet)
@@ -60,13 +63,21 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate, UINavi
             }
             else {
                 if let videoURL = info[UIImagePickerController.InfoKey.mediaURL] as? URL {
-                    UISaveVideoAtPathToSavedPhotosAlbum(videoURL.relativePath, nil, nil, nil)
+                    UserDefaults.standard.set(videoURL, forKey: "Selected_Video")
+                    UserDefaults.standard.synchronize()
+                    
+                    //This code is save Video to album.
+                    //UISaveVideoAtPathToSavedPhotosAlbum(videoURL.relativePath, nil, nil, nil)
                 }
             }
             dismiss(animated: true, completion: nil)
         }
     }
     
+    
+}
+
+extension UIViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
 }
 
